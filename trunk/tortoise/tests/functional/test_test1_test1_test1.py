b6ec46ac0401fc9bc8a4b88d75be6c35dfd8aefe
zhangjiayin99 <zhangjiayin99@c96f2ed2-2b72-e25c-2f08-87088423c85c>
2010-05-21 10:37:13 +0000

    add test

---
+++ trunk/tortoise/tests/functional/test_test1_test1_test1.py
@@ -0,0 +1,7 @@
+from tortoise.tests import *
+
+class TestTest1Controller(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='test1/test1/test1', action='index'))
+        # Test response...
